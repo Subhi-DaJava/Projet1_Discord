5ea135456ef7e452fef7aecdb4425289f8e92277
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-14 16:34:14 +0200

    Initial commit: Tunji sinaq commit -->Subhy

---
+++ src/Projet_1.java
@@ -0,0 +1,17 @@
+public class Projet_1 {
+    /**
+     * Bu programm 5 qitim Uyghur sozini bisip chiqiridu;
+     * @author
+     */
+    public static void main(String[] args) {
+
+        //10 qitim toluq aylinidu, i peqet jup san(0 mu oz ichide) bolghandila andin Uyghur sozini bisip chiqiridu;
+
+        for(int i=0; i<10; i+){
+            if(i%2=0){
+                System.out.println(i+" Uyghur");
+            }
+        }
+    }
+
+}
